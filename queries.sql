9ae20dc7b528b4a2a16d2a9e76413b834f2131c6
hectorchu~ <hector.chu828@gmail.com>
2022-02-01 13:37:42 -0800

    Finished 2/3 exercises

--- queries.sql
+++ queries.sql
@@ -1,6 +1,23 @@
 -- write your queries here
-select * from vehicles
-right outer join owners
-on owners.id = vehicles.owner_id
-order by vehicles.owner_id
+SELECT *
+FROM owners
+LEFT JOIN vehicles
+ON owners.id = vehicles.owner_id
+ORDER BY owners.id;
+
+SELECT owners.first_name, COUNT(vehicles.owner_id)
+FROM owners
+JOIN vehicles
+ON owners.id = vehicles.owner_id
+GROUP BY first_name
+ORDER BY count;
+
+ first_name | count
+------------+-------
+ Alex       |     1
+ Jane       |     2
+ Bob        |     2
+ Melody     |     2
+ Sarah      |     3
+ Shana      |     4
 
